00f446c518471f8b3209de26b74aaab237b5012a
imanseng <imandwisatrio99@gmail.com>
2026-03-03 14:09:59 +0700

    first commit

---
+++ src/preTest.java
@@ -0,0 +1,5 @@
+public class preTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("======================\nKARTU TANDA MAHASISWA\nNama\t: Iman Dwi Satrio\nNIM\t: 105224029\nProdi\t: Ilmu Komputer \n======================");
+    }
+}
